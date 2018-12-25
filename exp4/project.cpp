02a6cc33879a7eb9faf7e7487234c1bb47e8270f
paiforsyth <paiforsyth@hotmail.com>
2018-12-25 14:47:29 -0500

    1

---
+++ exp4/project.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include <arrayfire.h>
+#include <flashlight/flashlight.h>
+
+/**
+ * ###### #         ##    ####  #    # #      #  ####  #    # #####
+ * #      #        #  #  #      #    # #      # #    # #    #   #
+ * #####  #       #    #  ####  ###### #      # #      ######   #
+ * #      #       ######      # #    # #      # #  ### #    #   #
+ * #      #       #    # #    # #    # #      # #    # #    #   #
+ * #      ####### #    #  ####  #    # ###### #  ####  #    #   #
+ */
+int main() {
+  const int nSamples = 10000;
+  const int nFeat = 10;
+  auto X = af::randu(nFeat,nSamples) +1;
+  auto signal = af::sum(af::pow(X,3),0).T();
+  auto noise =  af::sin(2*M_PI *af::randu(nSamples));
+  //af::print("signal", signal);
+  //af::print("noise",noise);
+  auto Y = signal + noise;
+  fl::TensorDataset ds({X,Y} )
+  
+  
+  return 0;
+}
