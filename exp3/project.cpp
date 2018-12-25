e60586a1b5c64ff740bcc72bad4cb23c6eb7a32a
paiforsyth <paiforsyth@hotmail.com>
2018-12-25 13:53:28 -0500

    af

--- exp3/project.cpp
+++ exp3/project.cpp
@@ -15,6 +15,6 @@ int main() {
   const int nSamples = 10000;
   const int nFeat = 10;
   auto X = af::randu(nFeat,nSamples) +1;
-  auto Y = af::sum(aff::pow(X,3),0) + af::sin(2*M_PI *af::randu(nSamples));
+  auto Y = af::sum(af::pow(X,3),0) + af::sin(2*M_PI *af::randu(nSamples));
   return 0;
 }
